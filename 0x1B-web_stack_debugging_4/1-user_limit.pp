8bb64b6accc60321d1bc6e021841e22f25a24df3
Nelvine Isubire Kavaya <149570160+Xnelliek@users.noreply.github.com>
2024-11-14 09:47:13 +0300

    Add files via upload

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,7 @@
+#Changed OS configuration with an ability to login with the holberton user and open a file without any error message.
+
+file { 'loginFile':
+    ensure  => present,
+    path    => '/etc/security/limits.conf',
+    content => '#File erased'
+}
